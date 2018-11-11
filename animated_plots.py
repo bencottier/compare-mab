5219c5bd21bf90017f8ea8294c6d3860b980a473
Ben Cottier <bcottier2@gmail.com>
2018-11-11 12:39:27 +1000

    Change EXP3 plot to FPL plot

--- animated_plots.py
+++ animated_plots.py
@@ -19,29 +19,37 @@ def animate(i, anim, selections, results, values):
     sf = anim.section_frames 
 
     if j == anim.num_iter:  # End animation with neutral display
-        anim.value(values[:, j])
+        anim.value(values[:, :, j])
         return
     
     s = selections[j]
     r = results[j]
-    v = values[:, j]
+    v = values[:, :, j]  # barset, arms, iteration
     if i_iter < sf[0]:
         # Section 1: show current values
         if i_iter == 0:
             anim.value(v)
     elif i_iter < sf[1]:
-        # Section 2: indicate selection
-        if i_iter == sf[0]:
-            anim.select(s)
+        # Section 2: animate change in auxilliary value by interpolating
+        change = np.array([values[0, :, j + 1] - v[0], np.zeros_like(v[1])])
+        frame = i_iter - sf[0]
+        total_frames = sf[1] - sf[0]
+        v_anim = v
+        v_anim += frame * change / total_frames
+        anim.update(v_anim)
     elif i_iter < sf[2]:
-        # Section 3: indicate result of selection
+        # Section 3: indicate selection
         if i_iter == sf[1]:
+            anim.select(s)
+    elif i_iter < sf[3]:
+        # Section 4: indicate result of selection
+        if i_iter == sf[2]:
             anim.result(s, r)
-    else:  # i_iter >= sf[2]
-        # Section 4: animate change in value by interpolating
-        change = values[:, j + 1] - v
-        frame = i_iter - sf[2]
-        total_frames = sf[3] - sf[2]
+    else:  # i_iter >= sf[-1]
+        # Last section: animate change in value by interpolating
+        change = values[:, :, j + 1] - v
+        frame = i_iter - sf[-2]
+        total_frames = sf[-1] - sf[-2]
         v_anim = v
         v_anim += frame * change / total_frames
         anim.update(v_anim)
@@ -50,9 +58,9 @@ def animate(i, anim, selections, results, values):
 class Animation:
 
     COLOUR_DEFAULT = 'C0'  # 'C0' blue
-    COLOUR_SELECT  = 'C0'  # 'C1' orange
-    COLOUR_SUCCESS = 'C0'  # 'C2' greed
-    COLOUR_FAILURE = 'C0'  # 'C3' red
+    COLOUR_SELECT  = 'C1'  # 'C1' orange
+    COLOUR_SUCCESS = 'C2'  # 'C2' greed
+    COLOUR_FAILURE = 'C3'  # 'C3' red
 
     def __init__(self, selections, results, values, iter_start, iter_end,
             fps=30, speed=2):
@@ -65,11 +73,11 @@ class Animation:
             iter_end = len(selections)
         selections = selections[iter_start:iter_end]
         results = results[iter_start:iter_end]
-        values = values[:, iter_start:iter_end+1]
+        values = values[:, :, iter_start:iter_end+1]
         self.num_iter = iter_end - iter_start
 
         self.fps = fps  # desired output frame rate
-        section_times = 1./speed * np.array([1, 2, 3, 4])
+        section_times = 1./speed * np.array([1, 2, 3, 4, 5])
         self.section_frames = (self.fps * section_times).astype(np.int32)
         self.frames_per_iter = self.section_frames[-1]
         num_frames = self.frames_per_iter * (self.num_iter + 1)
@@ -102,44 +110,49 @@ class BarAnimation(Animation):
         super(BarAnimation, self).__init__(selections, results, values, 
                 iter_start, iter_end, fps, speed)
         if num_bars is None:
-            num_bars = values.shape[0]
+            num_bars = values.shape[1]
         self.x = np.arange(1, num_bars + 1)
         self.bar_sets = [plt.bar(self.x, np.zeros(num_bars)) 
                 for i in range(num_series)]
+        
+        for b in self.bar_sets[0]:
+            b.set_color('gray')
 
     def value(self, values):
-        for i, b in enumerate(self.bar_sets[0]):
-            b.set_color(self.COLOUR_DEFAULT)
-            b.set_height(values[i])
+        for i, bar_set in enumerate(self.bar_sets):
+            for j, b in enumerate(bar_set):
+                if i == 1:
+                    b.set_color(self.COLOUR_DEFAULT)
+                b.set_height(values[i][j])
 
     def select(self, selection):
-        self.bar_sets[0][selection].set_color(self.COLOUR_SELECT)
+        self.bar_sets[1][selection].set_color(self.COLOUR_SELECT)
 
     def result(self, selection, result):
         c = self.COLOUR_SUCCESS if result > 0 else self.COLOUR_FAILURE
-        self.bar_sets[0][selection].set_color(c)
+        self.bar_sets[1][selection].set_color(c)
 
     def update(self, values):
-        for i, b in enumerate(self.bar_sets[0]):
-            b.set_height(values[i])
+        for i, bar_set in enumerate(self.bar_sets):
+            for j, b in enumerate(bar_set):
+                b.set_height(values[i][j])
 
 
 if __name__ == '__main__':
 
-    from agents import Exp3Agent
+    from agents import EpsilonGreedyAgent, FPLAgent, Exp3Agent
     from bandits import Bandit
 
     bandit_probs = [0.10, 0.50, 0.60, 0.80, 0.10,
                     0.25, 0.60, 0.45, 0.75, 0.65]  # success probability
 
     bandit = Bandit(bandit_probs)
-    agent = Exp3Agent(bandit, 0.1)
+    agent = FPLAgent(bandit, 0.1)
     N_episodes = 1000
 
     action_history = np.zeros(N_episodes, dtype=np.int32)
     reward_history = np.zeros(N_episodes)
-    weight_history = np.ones((bandit.N, N_episodes + 1))
-    prob_history = 0.1 * np.ones((bandit.N, N_episodes + 1))
+    value_history = np.zeros((2, bandit.N, N_episodes + 1))
 
     for episode in range(N_episodes):
         # Choose action from agent (from current Q estimate)
@@ -151,34 +164,33 @@ if __name__ == '__main__':
         # Append to history
         action_history[episode] = action
         reward_history[episode] = reward
-        # value_history[:, episode + 1] = agent.ps
-        weight_history[:, episode + 1] = agent.ws
-        prob_history[:, episode] = agent.ps
+        value_history[0, :, episode + 1] = agent.Q + agent.z
+        value_history[1, :, episode + 1] = agent.Q
 
-    # anim_weight = BarAnimation(action_history, reward_history, weight_history, 
-    #         iter_start=0, iter_end=10, speed=4)
-
-    # ax = plt.gca()
-    # plt.ylim([0.95, 1.25])
-    # plt.xticks(range(1, bandit.N + 1))
-    # # ax.yticks([])
-    # plt.xlabel("action")
-    # plt.ylabel("weight")
-    # ax.yaxis.grid(True)
-
-    # # anim_weight.save("output/graphics/exp3_weight_bar.mp4")
-    # plt.show()
-
-    anim_prob = BarAnimation(action_history, reward_history, prob_history, 
-            iter_start=0, iter_end=10, speed=120)
+    anim = BarAnimation(action_history, reward_history, value_history, 
+            iter_start=150, iter_end=160, speed=2, num_series=2)
 
     ax = plt.gca()
-    plt.ylim([0.0, 0.4])
+    plt.ylim([0.0, 100.0])
     plt.xticks(range(1, bandit.N + 1))
     # ax.yticks([])
     plt.xlabel("action")
-    plt.ylabel("probability")
+    plt.ylabel("value")
+    # ax.yaxis.grid(True)
+
+    anim.save("output/graphics/fpl_q1.mp4")
+    # plt.show()
+
+    anim = BarAnimation(action_history, reward_history, value_history, 
+            iter_start=200, iter_end=210, speed=2, num_series=2)
+
+    ax = plt.gca()
+    plt.ylim([0.0, 100.0])
+    plt.xticks(range(1, bandit.N + 1))
+    # ax.yticks([])
+    plt.xlabel("action")
+    plt.ylabel("value")
     # ax.yaxis.grid(True)
     
-    # anim_prob.save("output/graphics/exp3_prob_bar_fast.mp4")
-    plt.show()
+    anim.save("output/graphics/fpl_q2.mp4")
+    # plt.show()
